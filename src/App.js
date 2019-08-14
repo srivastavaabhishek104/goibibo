297884bab06ebfa9e3c56dfa20a1b865661b8a31
unknown <srivastavaabhishek104@gmail.com>
2019-08-14 12:57:54 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import {Route,Switch} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Help from './components/Help/Help';
+import BookingHome from './components/BookingHome/BookingHome';
+import YourBooking from './components/YourBooking/YourBooking';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Layout>
+					<Switch>
+						<Route path="/" exact strict component={Home} />
+						<Route path="/about" exact strict component={About} />
+						<Route path="/contact" exact strict component={Contact} />
+						<Route path="/help" exact strict component={Help} />
+						<Route path="/dashboard" exact strict component={BookingHome} />
+						<Route path="/bookings" exact strict component={YourBooking} />
+					</Switch>
+				</Layout>		
+			</div>
+		);
+	}
 }
 
 export default App;
